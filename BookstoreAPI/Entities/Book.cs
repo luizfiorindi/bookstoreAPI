d6428f4f82c7b6e5c449407ff5aa277202705c8c
Luiz Fiorindi <luiz_fiorindi@outlook.com>
2026-09-13 15:52:58 -0300

    Created Book entity

---
+++ BookstoreAPI/Entities/Book.cs
@@ -0,0 +1,39 @@
+namespace BookstoreAPI.Entities;
+
+public class Book
+{
+    public Guid Id { get; private set; }
+    public string Title { get; private set; } = string.Empty;
+    public string Author { get; private set; } = string.Empty;
+    public string Genre { get; private set; } = string.Empty;
+    public decimal Price { get; private set; }
+    public int Stock { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+    public DateTime? UpdatedAt { get; private set; }
+
+    public Book(string title, string author, string genre, decimal price, int stock)
+    {
+        Id = Guid.NewGuid();
+        Title = title;
+        Author = author;
+        Genre = genre;
+        Price = price;
+        Stock = stock;
+        CreatedAt = DateTime.UtcNow;
+    }
+
+    public void Update(
+        string title,
+        string author,
+        string genre,
+        decimal price,
+        int stock)
+    {
+        Title = title;
+        Author = author;
+        Genre = genre;
+        Price = price;
+        Stock = stock;
+        UpdatedAt = DateTime.UtcNow;
+    }
+}
